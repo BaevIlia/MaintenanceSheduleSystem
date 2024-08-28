ddc412472ccfa671d24a6d016a3fed76c2b86735
Ilia Baev <baevilia.201@yandex.ru>
2024-08-28 16:04:22 +0300

    В контекст добавлены DbSets

--- MaintenanceSheduleSystem.Persistance/ApplicationDbContext.cs
+++ MaintenanceSheduleSystem.Persistance/ApplicationDbContext.cs
@@ -4,12 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MaintenanceSheduleSystem.Persistance.Entities;
 
 namespace MaintenanceSheduleSystem.Persistance
 {
     internal class ApplicationDbContext : DbContext
     {
-        public DbSet<>
+        public DbSet<AdminEntity> Admins { get; set; }
+        public DbSet<InstrumentEntity> Instruments { get; set; }
+        public DbSet<MachineEntity> Machines { get; set; }
+        public DbSet<OrderEntity> Orders { get; set; }
+        public DbSet<PlannerEntity> Planners { get; set; }
+        public DbSet<ServiceEntity> Services { get; set; }
+        public DbSet<SparePartEntity> SpareParts { get; set; }
+        public DbSet<ServicesOrdersEntity> ServicesOrders { get; set; }
+        public DbSet<UserEntity> Users { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             
