d4aa896d3455a7b825c87d08c47cd79e281b6ae7
Ilia Baev <baevilia.201@yandex.ru>
2024-09-03 17:36:05 +0300

    Добавлен контроллер администратора

---
+++ MaintenanceSheduleSystem.API/Controllers/AdministratorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaintenanceSheduleSystem.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdministratorController : ControllerBase
+    {
+        public async Task<IActionResult> CreatePlannerEngineer() 
+        {
+            return Ok();
+        }
+
+        public async Task<IActionResult> CreateServiceman() 
+        {
+            return Ok();
+        }
+    }
+}
