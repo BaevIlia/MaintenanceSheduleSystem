8320502363cbffbf799d86aadae3cd5bb1107b74
Ilia Baev <baevilia.201@yandex.ru>
2024-09-03 11:16:21 +0300

    В тестовом эндпоинте добавлены методы создания пользователя, входа в аккаунт и метод-заглушка для тестирования доступов по ролям

--- MaintenanceSheduleSystem.API/Controllers/TestAPI.cs
+++ MaintenanceSheduleSystem.API/Controllers/TestAPI.cs
@@ -6,29 +6,22 @@ namespace MaintenanceSheduleSystem.API.Controllers
     [Route("[controller]")]
     public class TestAPI : ControllerBase
     {
-        [HttpGet("hello")]
-        public async Task<IActionResult> SayHello() 
+        [HttpPost("createUser")]
+        public IActionResult UserCreate()
         {
-            return Ok("Hello world");
-        }
-        [HttpPost("createAdmin")]
-        public async Task<IActionResult> CreateAdmin(string email, string password, string surname, string firstName, string lastName, string ownAcceptKey) 
-        {
-            User administrator = Administrator.Create(Guid.NewGuid(), email, password, new FullName(surname, firstName, lastName), ownAcceptKey);
-            return Ok(administrator);
-        }
-        [HttpPost("createPlanner")]
-        public async Task<IActionResult> CreatePlanner(string email, string password, string surname, string firstName, string lastName)
-        {
-            User administrator = PlannerEngineer.Create(Guid.NewGuid(), email, password, new FullName(surname, firstName, lastName));
-            return Ok(administrator);
-        }
-        [HttpPost("createService")]
-        public async Task<IActionResult> CreateService(string email, string password, string surname, string firstName, string lastName)
-        {
-            User administrator = Serviceman.Create(Guid.NewGuid(), email, password, new FullName(surname, firstName, lastName), null);
-            return Ok(administrator);
+            return Ok();
         }
 
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+
+        [HttpGet("role")]
+        public IActionResult CheckRole() 
+        {
+            return Ok();
+        }
     }
 }
